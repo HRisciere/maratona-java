da56cd600e0efe0fb8592685efc7e38f284936d8
Henrique Risciere <henriquerisc@gmail.com>
2025-05-30 19:23:34 -0300

    feat(MangaByIdComparator): add comparator to sort manga by id

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MangaSortTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MangaSortTest01.java
@@ -4,7 +4,26 @@ import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Manga;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
+//Agora eu quero ordenar por ID, em um específico ponto do meu software, então eu não vou utilizar o comparable em
+// uma classe, igual foi feito na classe Manga, eu vou utilizar o COMPARATOR. Eu preciso ter um objeto que passa no
+// teste "é um, COMPARATOR"
+// Tenho que falar em qual classe eu estou fazendo a comparação então eu coloco Comparator<Manga> e faço a sobrescrita
+// do commpare.
+
+class MangaByIdComparator implements Comparator<Manga> {
+
+    @Override
+    public int compare(Manga manga1, Manga manga2) {
+//        Como eu não estou no OBJETO manga, eu estou no OBJETO MangaByIdComparator, então eu tenho acesso a manga1 e
+//        manga2. Quem vai mandar o manga1 e manga2? O próprio JAVA.
+//        Só sei que eu quero ordernar por ID, então eu faço o que eu mesmo coloquei abaixo
+        return manga1.getId().compareTo(manga2.getId());
+    }
+}
+
 
 public class MangaSortTest01 {
     public static void main(String[] args) {
@@ -18,6 +37,17 @@ public class MangaSortTest01 {
             System.out.println(manga);
         }
         Collections.sort(mangas);
+        System.out.println("-------------------");
+        for (Manga manga : mangas) {
+            System.out.println(manga);
+        }
+
+
+//      Fazendo a comparação por ID, esse método é sobrecarregado
+//      Quem vai se encarregar de chamar os objetos é o próprio Java
+//      Collections.sort(mangas, new MangaByIdComparator()); Posso fazer desta mandeira ou utilizando a de baixo
+//      Tem maneiras mais simples de fazer isso, utilizando classes anônimas, mas ainda não foi ensinado
+        mangas.sort(new MangaByIdComparator()); // Neste caso a própria LISTA mangas está fazendo a ordenação
         System.out.println("-------------------");
         for (Manga manga : mangas) {
             System.out.println(manga);
